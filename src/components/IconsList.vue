6cd242239634daf5f6b036607fe39107e41f2485
Hoang Do <nkhdo94@gmail.com>
2023-07-06 15:48:05 +0700

    feat: scroll to section when click on section header

--- src/components/IconsList.vue
+++ src/components/IconsList.vue
@@ -13,6 +13,7 @@ const emit = defineEmits<{
   (e: 'select', value: string): void
 }>()
 
+const scrollContainerRef = ref<HTMLDivElement>()
 
 const emojsSections = computed(() => {
   const hIconSection = {
@@ -37,6 +38,16 @@ const activeSection = ref(emojsSections.value[0].name)
 function select(icon: string) {
   emit('select', icon)
 }
+
+function scrollToSection(name: string) {
+  const sectionEl = scrollContainerRef.value?.querySelector(`[data-section-name="${name}"]`)
+  if (sectionEl) {
+    sectionEl.scrollIntoView({
+      behavior: 'smooth'
+    })
+    activeSection.value = name
+  }
+}
 </script>
 
 <template>
@@ -48,14 +59,15 @@ function select(icon: string) {
         :key="section.name"
         class="tw-cursor-pointer tw-p-1"
         :class="{ 'tw-border-blue-600 tw-border-b-2': activeSection === section.name }"
+        @click="scrollToSection(section.name)"
       >
         {{ section.label }}
       </div>
     </div>
 
     <!-- body -->
-    <div class="tw-max-h-80 tw-overflow-auto tw-p-2">
-      <div v-for="section in emojsSections" :key="section.name">
+    <div class="tw-max-h-80 tw-overflow-auto tw-p-2" ref="scrollContainerRef">
+      <div v-for="section in emojsSections" :key="section.name" :data-section-name="section.name">
         <div class="tw-font-semibold">
           {{ section.name }}
         </div>
